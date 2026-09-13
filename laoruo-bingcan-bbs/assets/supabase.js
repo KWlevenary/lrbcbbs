31fd5de15feafd96883e062e5782be78993d91b5
KWlevenary <wyzyymy114514@outlook.com>
2026-09-13 12:19:05 +0800

    Delete laoruo-bingcan-bbs directory

--- laoruo-bingcan-bbs/assets/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-export const SUPABASE_URL = 'https://lnfjgsycfevgrxhgefoi.supabase.co'
-export const SUPABASE_ANON_KEY = 'sb_publishable_7LpIAS78VIY_DD1LJhraLg_-QlBqyRM'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
